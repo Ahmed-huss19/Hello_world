0f7f30d0ec79ad6ed58b30dcaa87f49e41fb7f0e
Ahmed-huss19 <126443901+Ahmed-huss19@users.noreply.github.com>
2023-06-10 22:58:42 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, flash
+app= Flask(__name__)
+app.secret_key="Ahmed"
+@app.route("/hello")
+def index():
+    flash("What's your name?")
+    return render_template("index.html")
+@app.route("/greet",methods=["POST","GET"])
+def greet():
+    flash("Hi"+" "+ str(request.form['name_input'])+","+ "Great to see you")
+    return render_template("index.html")
